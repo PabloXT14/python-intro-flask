68daa5c8085ba94a9cd5e56350dda5226c1e214a
Pablo Alan <pabloxt14@gmail.com>
2024-01-27 16:49:02 -0300

    feat: created User model/table

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_login import UserMixin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
@@ -13,6 +14,32 @@ CORS(app)
 
 
 # Modelagem (modelando o formato que os nosso dados terá para ir para o banco de dados)
+'''
+# Comandos de modelagem (no terminal):
+- flask shell (ativa o terminal embutido do flask para aceitar comandos para nossa aplicação)
+- db.create_all() (cria os nosso models/tables em nosso banco de dados configurado)
+- db.drop_all() (exclui ou desfaz todas alterações que realizamos em nosso banco)
+- db.session.add() (para adicionar algum registro a alguma tabela no banco de dados)
+- db.session.commit() (para nossas alterações feitas serem de fato commitadas no banco de dados)
+- exit() (sai do terminal integrado do flask)
+
+* obs: você também pode realizar criação de registros em uma determinada table no banco através do próprio terminal integrado do flask, ex:
+>>> user = User(username="admin", password="123")
+>>> db.session.add(user)
+>>> db.session.commit()
+
+* dica: sempre que criar um novo model execute os comandos db.drop_all(), db.create_all() e db.session.commit() respectivamente, para resetar o banco de dados e não haver conflito com as novas tabelas caso estar apresentem algum tipo de relação.
+'''
+
+
+# User (id, username, password)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+
+# Produto (id, name, price, description)
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
