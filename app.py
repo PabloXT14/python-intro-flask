7bd8af2daaa9c302606eef3c730bf1271e0ace9c
Pablo Alan <pabloxt14@gmail.com>
2024-01-28 19:17:49 -0300

    feat: create CartItem model

--- app.py
+++ app.py
@@ -38,20 +38,26 @@ CORS(app)
 '''
 
 
-# User (id, username, password)
+# User Model (id, username, password)
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
+    cart = db.relationship('CartItem', backref='user', lazy=True)
 
-
-# Produto (id, name, price, description)
+# Produto Model (id, name, price, description)
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
+# CartItem Model (id, product_id, user_id)
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # referência ao campo id do model User
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False) # referência ao campo id do model Product
+ 
 
 # Authentication Middleware
 @login_manager.user_loader
